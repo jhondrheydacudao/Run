2e78ab044a41e2ac4bbb2fdffef877c10f08b8fd
jhondrhey/developer's <166288970+jhondrheydacudao@users.noreply.github.com>
2025-03-13 17:11:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Executorx Hosting is running!');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
